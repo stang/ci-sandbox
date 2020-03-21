a322b3238a118b08f87f6d9eb4d03f55a1c8bd68
Stephane Tang <hi@stang.sh>
2020-03-21 18:17:38 +0000

    say hi to everyone

--- internal/lib/main.go
+++ internal/lib/main.go
@@ -7,6 +7,9 @@ import (
 
 // Greetings takes a name as single argument and return a greeting for that name
 func Greetings(name string) string {
+	if name == "" {
+		name = "everyone"
+	}
 	msg := fmt.Sprintf("Hello %s!", strings.Title(name))
 	return msg
 }
